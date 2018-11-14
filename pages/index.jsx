13b89fa770d4b22f166c5134cc88aaaffdd04b29
davodaslanifakor <davodaslanifakor@gmail.com>
2018-11-14 16:48:10 +0330

    add slide show

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
 import Layout from 'components/layout/default'
+import Slider from 'components/ui/slideshow/slider'
 class Home extends Component {
     render() { 
         return (   
           <Layout>
-            <p>home</p>
+            <Slider/>
           </Layout>);
     }
 }
